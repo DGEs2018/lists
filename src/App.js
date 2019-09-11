6658bf5ccd39e3aea2ec50a0a3ddcd90679373e1
Dawit <dawit@ginetta.net>
2019-09-11 14:54:34 +0200

    Add input field to the App.js, create components directory and a component called ValidationComponent inside

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const inputChangeHandler = () => {
+		return e.target.value.length;
+	};
+	return (
+		<div className="App">
+			<input id="input" type="text" placeholder="input" onChange={inputChangeHandler} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ValidationComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ValidationComponent = () => {
+	return <div />;
+};
+
+export default ValidationComponent;
